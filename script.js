3bf9f013fed13be99d5c9dcf605baa5dc15d155f
killshotxd <hassanansari211@gmail.com>
2022-03-16 16:31:26 +0530

    btoa algo implemented

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const input = document.querySelector("input");
+  const input = document.querySelector("#message-input");
 
-  console.log(input.value);
+  const encrypted = btoa(input.value);
+
+  document.querySelector("#link-input").value = encrypted;
 });
